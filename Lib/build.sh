9f7e8d9f6abb5a6f697b2d031e85473ffb8bac81
karry520 <884816926@qq.com>
2020-12-17 20:09:08 +0800

    make list

--- Lib/build.sh
+++ Lib/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-rm KD_lib*.so
+rm KD_lib/KD*.so
 echo "rm KD.so!"
 cd build 
 #make clean 
 cmake -DCMAKE_POSITION_INDEPENDENT_CODE=ON ..
 make -j8
 
-cp lib/KD_lib*.so ../
+cp lib/KD_lib*.so ../KD_lib/
 echo "cp KD.so!"
 cd ../
 # python $1
